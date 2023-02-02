35a9f075ba2c3734868a7b911b6ff293487359ac
R <raghad.fayez1@gmail.com>
2023-02-02 20:39:57 +0300

    Initial Commit

---
+++ quill/quillApp.swift
@@ -0,0 +1,17 @@
+//
+//  quillApp.swift
+//  quill
+//
+//  Created by Raghad Fayez on 11/07/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct quillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
